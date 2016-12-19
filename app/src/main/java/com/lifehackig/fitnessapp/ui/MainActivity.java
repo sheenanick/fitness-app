9decb3a1ccfaeb933a3aecfa9a9744a38ab46c67
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 12:43:53 -0800

    set text to user display name

--- app/src/main/java/com/lifehackig/fitnessapp/ui/MainActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/MainActivity.java
@@ -12,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.lifehackig.fitnessapp.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.textView) TextView mTextView;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mTextView.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
     }
 
     @Override
