f581c52f021fe9329397e9bc1c0e3bdf22dc9c83
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 15:55:46 -0800

    add constructor and getters for Exercise model

--- app/src/main/java/com/lifehackig/fitnessapp/models/Exercise.java
+++ app/src/main/java/com/lifehackig/fitnessapp/models/Exercise.java
@@ -1,4 +1,45 @@
 package com.lifehackig.fitnessapp.models;
 
 public class Exercise {
+    private String name;
+    private Integer reps;
+    private Integer duration;
+    private Integer weight;
+    private String muscle;
+    private Integer calories;
+
+    public Exercise() {}
+
+    public Exercise(String name, Integer reps, Integer duration, Integer weight, String muscle, Integer calories) {
+        this.name = name;
+        this.reps = reps;
+        this.duration = duration;
+        this.weight = weight;
+        this.muscle = muscle;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getReps() {
+        return reps;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
 }
