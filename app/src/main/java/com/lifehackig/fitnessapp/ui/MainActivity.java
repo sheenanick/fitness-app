139914dec45974b4d7be3716395205e28fe8d8f7
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 11:29:53 -0800

    add log out functionality and menu

--- app/src/main/java/com/lifehackig/fitnessapp/ui/MainActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/MainActivity.java
@@ -1,15 +1,51 @@
 package com.lifehackig.fitnessapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lifehackig.fitnessapp.R;
 
+import butterknife.Bind;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.textView) TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_log_out:
+                logout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
