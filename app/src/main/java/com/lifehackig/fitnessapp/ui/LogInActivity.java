f2af846eaa0a2b5c740d29243c8172d13f94f9c8
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 12:54:39 -0800

    add log in functionality

--- app/src/main/java/com/lifehackig/fitnessapp/ui/LogInActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/LogInActivity.java
@@ -1,11 +1,21 @@
 package com.lifehackig.fitnessapp.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lifehackig.fitnessapp.R;
 
 import butterknife.Bind;
@@ -13,13 +23,39 @@ import butterknife.ButterKnife;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.signUpText) TextView mSignUp;
+    @Bind(R.id.email) EditText mEmail;
+    @Bind(R.id.password) EditText mPassword;
+    @Bind(R.id.logInButton) Button mLogInButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+
+        mLogInButton.setOnClickListener(this);
         mSignUp.setOnClickListener(this);
+    }
+
+    public void createAuthStateListener() {
+        mAuthStateListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 
     @Override
@@ -28,5 +64,42 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
             startActivity(intent);
         }
+        if (v == mLogInButton) {
+            logInWithPassword();
+        }
+    }
+
+    public void logInWithPassword() {
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        if (email.equals("")) {
+            mEmail.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPassword.setError("Please enter your password");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(LogInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListner);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthStateListner);
     }
 }
