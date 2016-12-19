8afa7902359e10e370000380932e8391e628253f
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 12:59:13 -0800

    add progress dialog

--- app/src/main/java/com/lifehackig/fitnessapp/ui/LogInActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/LogInActivity.java
@@ -1,10 +1,10 @@
 package com.lifehackig.fitnessapp.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +28,8 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.logInButton) Button mLogInButton;
 
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListner;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +39,21 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
         mAuth = FirebaseAuth.getInstance();
         createAuthStateListener();
+        createProgressDialog();
 
         mLogInButton.setOnClickListener(this);
         mSignUp.setOnClickListener(this);
     }
 
+    private void createProgressDialog() {
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("Loading...");
+        mProgressDialog.setMessage("Authenticating with Firebase...");
+        mProgressDialog.setCancelable(false);
+    }
+
     public void createAuthStateListener() {
-        mAuthStateListner = new FirebaseAuth.AuthStateListener() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -72,18 +81,22 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void logInWithPassword() {
         String email = mEmail.getText().toString().trim();
         String password = mPassword.getText().toString().trim();
-        if (email.equals("")) {
+        boolean invalidEmail = email.equals("");
+        boolean invalidPassword = password.equals("");
+        if (invalidEmail) {
             mEmail.setError("Please enter your email");
-            return;
         }
-        if (password.equals("")) {
+        if (invalidPassword) {
             mPassword.setError("Please enter your password");
-            return;
         }
+        if (invalidEmail || invalidPassword) return;
+
+        mProgressDialog.show();
 
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                mProgressDialog.dismiss();
                 if (!task.isSuccessful()) {
                     Toast.makeText(LogInActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                 }
@@ -94,12 +107,12 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthStateListner);
+        mAuth.addAuthStateListener(mAuthStateListener);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mAuth.removeAuthStateListener(mAuthStateListner);
+        mAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
