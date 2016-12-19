b506e1b38fc587bbc3ca902707ba58afe972ce9c
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 15:56:09 -0800

    create New Exercise Activity

---
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/NewExerciseActivity.java
@@ -0,0 +1,15 @@
+package com.lifehackig.fitnessapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lifehackig.fitnessapp.R;
+
+public class NewExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_exercise);
+    }
+}
