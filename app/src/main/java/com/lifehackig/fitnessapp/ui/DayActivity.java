a5b376571596b59e0d1ad837ea811cd113d965f5
Sheena Nickerson <sheenanick@gmail.com>
2016-12-19 15:42:57 -0800

    add menu and set title to date

--- app/src/main/java/com/lifehackig/fitnessapp/ui/DayActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/DayActivity.java
@@ -3,8 +3,11 @@ package com.lifehackig.fitnessapp.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lifehackig.fitnessapp.R;
 
 public class DayActivity extends AppCompatActivity {
@@ -18,7 +21,34 @@ public class DayActivity extends AppCompatActivity {
         int year = intent.getIntExtra("year", 0);
         int month = intent.getIntExtra("month", 0);
         int day = intent.getIntExtra("day", 0);
+        String date = month + "/" + day + "/" + year;
+        getSupportActionBar().setTitle(date);
+    }
 
-        Log.d("date", month + "/" + day + "/" + year);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_log_out:
+                logout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(DayActivity.this, LogInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
