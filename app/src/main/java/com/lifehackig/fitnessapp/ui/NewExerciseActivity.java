91f90dfd9af457aaeb740a5f51019b064d6fc2ea
Sheena Nickerson <sheenanick@gmail.com>
2016-12-20 09:06:42 -0800

    save exercise to Firebase and reroute back to Day Activity

--- app/src/main/java/com/lifehackig/fitnessapp/ui/NewExerciseActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/NewExerciseActivity.java
@@ -1,6 +1,7 @@
 package com.lifehackig.fitnessapp.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,12 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lifehackig.fitnessapp.R;
+import com.lifehackig.fitnessapp.models.Exercise;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,13 +28,17 @@ public class NewExerciseActivity extends AppCompatActivity implements View.OnCli
     @Bind(R.id.reps) RadioButton mReps;
     @Bind(R.id.minutes) RadioButton mMinutes;
     @Bind(R.id.weightEditText) EditText mWeight;
-    @Bind(R.id.calories) EditText mCalories;
+    @Bind(R.id.caloriesEditText) EditText mCalories;
     @Bind(R.id.muscleSpinner) Spinner mMuscleSpinner;
     @Bind(R.id.addButton) Button mAddButton;
 
-    private int mYear;
-    private int mMonth;
-    private int mDay;
+    private Integer mYear;
+    private Integer mMonth;
+    private Integer mDay;
+
+    private String mCurrentUid;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     private String mMuscle;
 
@@ -50,6 +60,17 @@ public class NewExerciseActivity extends AppCompatActivity implements View.OnCli
         mMuscleSpinner.setOnItemSelectedListener(this);
 
         mAddButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    mCurrentUid = user.getUid();
+                }
+            }
+        };
     }
 
 
@@ -60,14 +81,39 @@ public class NewExerciseActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
-    private void saveNewExercise() {
+    public void saveNewExercise() {
         String name = mExerciseName.getText().toString().trim();
         Integer repsOrDuration = Integer.parseInt(mRepsOrDuration.getText().toString().trim());
-        boolean reps = mReps.isChecked();
-        boolean minutes = mMinutes.isChecked();
-        Integer weight = Integer.parseInt(mWeight.getText().toString().trim());
+        boolean isReps = mReps.isChecked();
+        Integer reps = 0;
+        Integer minutes = 0;
         Integer calories = Integer.parseInt(mCalories.getText().toString().trim());
 
+        String weight = mWeight.getText().toString().trim();
+        Integer intWeight = 0;
+        if (!weight.equals("")) {
+            intWeight = Integer.parseInt(weight);
+        }
+
+        if (isReps) {
+            reps = repsOrDuration;
+        } else {
+            minutes = repsOrDuration;
+        }
+
+        DatabaseReference dateRef = FirebaseDatabase.getInstance().getReference("members").child(mCurrentUid).child(mMonth.toString() + mDay.toString() + mYear.toString());
+        DatabaseReference exerciseRef = dateRef.push();
+        String pushId = exerciseRef.getKey();
+        Exercise exercise = new Exercise(name, reps, minutes, intWeight, mMuscle, calories, pushId);
+        exerciseRef.setValue(exercise);
+
+        Intent intent = new Intent(NewExerciseActivity.this, DayActivity.class);
+        intent.putExtra("year", mYear);
+        intent.putExtra("month", mMonth);
+        intent.putExtra("day", mDay);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 
 
@@ -80,6 +126,17 @@ public class NewExerciseActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
