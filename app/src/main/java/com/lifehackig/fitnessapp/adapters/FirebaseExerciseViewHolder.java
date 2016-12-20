327cd48da50aac62da2fd387ffd188ae258832ba
Sheena Nickerson <sheenanick@gmail.com>
2016-12-20 10:29:33 -0800

    add View Holder to use for Recycler View in Day Activity

---
+++ app/src/main/java/com/lifehackig/fitnessapp/adapters/FirebaseExerciseViewHolder.java
@@ -0,0 +1,28 @@
+package com.lifehackig.fitnessapp.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lifehackig.fitnessapp.R;
+import com.lifehackig.fitnessapp.models.Exercise;
+
+public class FirebaseExerciseViewHolder extends RecyclerView.ViewHolder {
+    private View mView;
+    private Context mContext;
+
+    public FirebaseExerciseViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+    }
+
+    public void bindExercise(Exercise exercise) {
+        TextView exerciseName = (TextView) mView.findViewById(R.id.name);
+        TextView calories = (TextView) mView.findViewById(R.id.calories);
+
+        exerciseName.setText(exercise.getName());
+        calories.setText(exercise.getCalories());
+    }
+}
