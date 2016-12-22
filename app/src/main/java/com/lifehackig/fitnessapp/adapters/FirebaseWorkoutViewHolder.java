bdc2e3e1781ce7d660fbacd4e3e668fd6d885cf5
Sheena Nickerson <sheenanick@gmail.com>
2016-12-22 12:17:15 -0800

    add click listener and temporary Toast until Details Activity is made

--- app/src/main/java/com/lifehackig/fitnessapp/adapters/FirebaseWorkoutViewHolder.java
+++ app/src/main/java/com/lifehackig/fitnessapp/adapters/FirebaseWorkoutViewHolder.java
@@ -4,14 +4,15 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lifehackig.fitnessapp.R;
 import com.lifehackig.fitnessapp.models.Workout;
 
-
-public class FirebaseWorkoutViewHolder extends RecyclerView.ViewHolder {
+public class FirebaseWorkoutViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     private View mView;
     private Context mContext;
+    private TextView mDetails;
 
     public FirebaseWorkoutViewHolder(View itemView) {
         super(itemView);
@@ -21,6 +22,16 @@ public class FirebaseWorkoutViewHolder extends RecyclerView.ViewHolder {
 
     public void bindWorkout(Workout workout) {
         TextView workoutName = (TextView) mView.findViewById(R.id.workoutName);
+        mDetails = (TextView) mView.findViewById(R.id.details);
+
         workoutName.setText(workout.getName());
+        mDetails.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mDetails) {
+            Toast.makeText(mContext, "Details Coming Soon!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
