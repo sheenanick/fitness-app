dafa6bbb1cb24fd0280f8a2b3162ffba95b239a2
Sheena Nickerson <sheenanick@gmail.com>
2017-01-13 16:50:03 -0800

    start Workout Details Activity on click and remove temporary Toast

--- app/src/main/java/com/lifehackig/fitnessapp/adapters/FirebaseWorkoutViewHolder.java
+++ app/src/main/java/com/lifehackig/fitnessapp/adapters/FirebaseWorkoutViewHolder.java
@@ -1,18 +1,21 @@
 package com.lifehackig.fitnessapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.lifehackig.fitnessapp.R;
 import com.lifehackig.fitnessapp.models.Workout;
+import com.lifehackig.fitnessapp.ui.WorkoutDetailsActivity;
 
 public class FirebaseWorkoutViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     private View mView;
     private Context mContext;
     private TextView mDetails;
+    private String mWorkoutId;
+    private String mWorkoutName;
 
     public FirebaseWorkoutViewHolder(View itemView) {
         super(itemView);
@@ -23,7 +26,8 @@ public class FirebaseWorkoutViewHolder extends RecyclerView.ViewHolder implement
     public void bindWorkout(Workout workout) {
         TextView workoutName = (TextView) mView.findViewById(R.id.workoutName);
         mDetails = (TextView) mView.findViewById(R.id.details);
-
+        mWorkoutId = workout.getPushId();
+        mWorkoutName = workout.getName();
         workoutName.setText(workout.getName());
         mDetails.setOnClickListener(this);
     }
@@ -31,7 +35,10 @@ public class FirebaseWorkoutViewHolder extends RecyclerView.ViewHolder implement
     @Override
     public void onClick(View v) {
         if (v == mDetails) {
-            Toast.makeText(mContext, "Details Coming Soon!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(mContext, WorkoutDetailsActivity.class);
+            intent.putExtra("workoutId", mWorkoutId);
+            intent.putExtra("workoutName", mWorkoutName);
+            mContext.startActivity(intent);
         }
     }
 }
