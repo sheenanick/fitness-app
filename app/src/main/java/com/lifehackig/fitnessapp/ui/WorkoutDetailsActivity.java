032e068c0c7fe065e293fa085865eba6a0740649
Sheena Nickerson <sheenanick@gmail.com>
2017-01-13 16:39:09 -0800

    add Workout Details Activity

---
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/WorkoutDetailsActivity.java
@@ -0,0 +1,138 @@
+package com.lifehackig.fitnessapp.ui;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.lifehackig.fitnessapp.R;
+import com.lifehackig.fitnessapp.adapters.FirebaseExerciseViewHolder;
+import com.lifehackig.fitnessapp.models.Exercise;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class WorkoutDetailsActivity extends AppCompatActivity {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    @Bind(R.id.bottom_navigation) BottomNavigationView mBottomNavigationView;
+
+    private String mCurrentUid;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseRecyclerAdapter mAdapter;
+    private DatabaseReference mExercises;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_details);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        final String workoutId = intent.getStringExtra("workoutId");
+        String workoutName = intent.getStringExtra("workoutName");
+        getSupportActionBar().setTitle(workoutName);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    mCurrentUid = user.getUid();
+                    mExercises = FirebaseDatabase.getInstance().getReference("workouts").child(mCurrentUid).child(workoutId).child("exercises");
+
+                    setupFirebaseAdapter();
+                }
+            }
+        };
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        Intent homeIntent = new Intent(WorkoutDetailsActivity.this, MainActivity.class);
+                        startActivity(homeIntent);
+                        break;
+                    case R.id.action_workouts:
+                        Intent workoutIntent = new Intent(WorkoutDetailsActivity.this, WorkoutsActivity.class);
+                        startActivity(workoutIntent);
+                        break;
+                    case R.id.action_account:
+                        Intent accountIntent = new Intent(WorkoutDetailsActivity.this, AccountActivity.class);
+                        startActivity(accountIntent);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setupFirebaseAdapter() {
+        mAdapter = new FirebaseRecyclerAdapter<Exercise, FirebaseExerciseViewHolder>(Exercise.class, R.layout.exercise_list_item, FirebaseExerciseViewHolder.class, mExercises) {
+            @Override
+            protected void populateViewHolder(FirebaseExerciseViewHolder viewHolder, Exercise model, int position) {
+                viewHolder.bindExercise(model);
+            }
+        };
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_log_out:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(WorkoutDetailsActivity.this, LogInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mAdapter.cleanup();
+    }
+
+}
