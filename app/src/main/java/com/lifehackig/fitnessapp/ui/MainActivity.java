818f5f1d899ed03d7102b820afa7b0fbb6c4dc65
Sheena Nickerson <sheenanick@gmail.com>
2017-01-29 15:36:21 -0800

    refactor to use Caldroid calendar

--- app/src/main/java/com/lifehackig/fitnessapp/ui/MainActivity.java
+++ app/src/main/java/com/lifehackig/fitnessapp/ui/MainActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,14 +26,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.lifehackig.fitnessapp.R;
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @Bind(R.id.calendarView) CalendarView mCalendarView;
     @Bind(R.id.date) TextView mDateTextView;
     @Bind(R.id.calories) TextView mCalories;
     @Bind(R.id.seeDetailsButton) Button mSeeDetailsButton;
@@ -41,9 +48,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private String mCurrentUid;
 
-    private Integer mYear;
-    private Integer mMonth;
-    private Integer mDay;
+    private DateFormat dateFormatter;
+    private DateFormat refIdFormatter;
+    private Date mDate;
+
+    private CaldroidFragment caldroidFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +60,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mYear = Calendar.getInstance().get(Calendar.YEAR);
-        mMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
-        mDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        String date = mMonth + "/" + mDay + "/" + mYear;
+        mDate = new Date();
+        dateFormatter = new SimpleDateFormat("MM/dd/yyyy");
+        String formattedDate = dateFormatter.format(mDate);
+        mDateTextView.setText(formattedDate);
 
-        mDateTextView.setText(date);
+        refIdFormatter = new SimpleDateFormat("MMddyyyy");
 
         mAuth = FirebaseAuth.getInstance();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -67,25 +76,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (user != null) {
                     getSupportActionBar().setTitle(user.getDisplayName());
                     mCurrentUid = user.getUid();
-                    String dateRefId = mMonth.toString() + mDay.toString() + mYear.toString();
+                    String dateRefId = refIdFormatter.format(mDate);
                     setCaloriesTextView(dateRefId);
                 }
             }
         };
 
-        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
-                mYear = year;
-                mMonth = month + 1;
-                mDay = day;
-                String date = mMonth + "/" + mDay + "/" + mYear;
-                mDateTextView.setText(date);
+        caldroidFragment = new CaldroidFragment();
+        // If Activity is created after rotation
+        if (savedInstanceState != null) {
+            caldroidFragment.restoreStatesFromKey(savedInstanceState,
+                    "CALDROID_SAVED_STATE");
+        }
+        // If activity is created from fresh
+        else {
+            Bundle args = new Bundle();
+            Calendar cal = Calendar.getInstance();
+            args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+            args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+            args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
+            caldroidFragment.setArguments(args);
+        }
 
-                String dateRefId = mMonth.toString() + mDay.toString() + mYear.toString();
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar, caldroidFragment);
+        t.commit();
+
+        caldroidFragment.setCaldroidListener(new CaldroidListener() {
+            @Override
+            public void onSelectDate(Date date, View view) {
+                mDate = date;
+                String formattedDate = dateFormatter.format(date);
+                mDateTextView.setText(formattedDate);
+
+                String dateRefId = refIdFormatter.format(date);
                 setCaloriesTextView(dateRefId);
             }
         });
+
 
         mSeeDetailsButton.setOnClickListener(this);
         mEditImage.setOnClickListener(this);
@@ -132,9 +160,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == mSeeDetailsButton) {
             Intent intent = new Intent(MainActivity.this, DayActivity.class);
-            intent.putExtra("year", mYear);
-            intent.putExtra("month", mMonth);
-            intent.putExtra("day", mDay);
+            DateFormat yearFormat = new SimpleDateFormat("yyyy");
+            DateFormat monthFormat = new SimpleDateFormat("MM");
+            DateFormat dayFormat = new SimpleDateFormat("dd");
+
+            intent.putExtra("year", yearFormat.format(mDate));
+            intent.putExtra("month", monthFormat.format(mDate));
+            intent.putExtra("day", dayFormat.format(mDate));
             startActivity(intent);
         }
         if (v == mEditImage) {
@@ -180,4 +212,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onStop();
         mAuth.removeAuthStateListener(mAuthStateListener);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (caldroidFragment != null) {
+            caldroidFragment.saveStatesToKey(outState, "CALDROID_SAVED_STATE");
+        }
+    }
+
 }
